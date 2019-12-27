2cbb8eb8dcecca849e7ea3ce8213ebe0cc1e3887
ZeeLog <Danjel.Zammit@gmail.com>
2019-12-27 22:44:41 +0100

    X00P: Build our own light HAL
    Change-Id: Id0ea4dbf5ac922abca059738f41f27b1f54c32a9
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>

--- device.mk
+++ device.mk
@@ -206,9 +206,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8937
+    android.hardware.light@2.0-service.X00P
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
