3899f81c91f18b4dca1ac9c798f003121a2a3371
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-02 02:15:53 +0200

    msm8937-common: Initial skeleton
    Change-Id: I18e7ff5334ca76e9b9b0022cb429bf2316724adf

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/asus/msm8937-common
+
+# Inherit the proprietary files
+include vendor/asus/msm8937-common/BoardConfigVendor.mk
