35fb4257a54a6d08354f15937d1873d7ec63677b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-02 16:04:01 +0200

    msm8937-common: Add cache as vendor support
    * 2018 phones doesn't have a vendor partition, but we have a oem partition which is 50MB, should be enough to be used as cache
    
    Change-Id: I6ce5fe7e3a2cf8462171d837dc03c4fc254f8c8b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -85,7 +85,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
+ifeq ($(TARGET_USES_CACHE_AS_VENDOR), true)
+LOCAL_SRC_FILES    := etc/fstab_legacy.qcom
+else
 LOCAL_SRC_FILES    := etc/fstab.qcom
+endif
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
