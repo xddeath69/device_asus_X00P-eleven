kardebayan/android_device_asus_msm8937-common

msm8937-common: Allow asserting baseband version
* Based off android_device_cyanogen_msm8916-common and android_device_xiaomi_sdm845-common

Co-authored-by: Michael Bestas <mikeioannina@gmail.com>
Change-Id: Ica08b05034407d5fd05db52b48e1f8448063a0b6

BoardConfigCommon.mk
@@ -186,6 +186,10 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_asus
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk

xddeath69/device_asus_X00P-eleven

BoardConfigCommon.mk
@@ -133,9 +133,13 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab_legacy.qcom
 else
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 endif
+TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_asus
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USES_MKE2FS := true
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2020-03-05