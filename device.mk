44f2b33616de2a71f04e0e3178288f725c6d1fcd
Jarl-Penguin <jarlpenguin@outlook.com>
2023-03-18 18:37:11 -0300

    X00P: Update thermal blobs to R
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ic4e795b3d60de50e736d4e0d55eba61e60d58613

--- device.mk
+++ device.mk
@@ -325,11 +325,6 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.msm8937
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
